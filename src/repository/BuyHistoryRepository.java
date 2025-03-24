6cf2e12d1fa3c547953e85b4d3ff4517a78a5875
bae juyeon <bjyon0415@gmail.com>
2025-03-24 17:57:06 +0900

    기초적인 BuyListRepository와 BuyHistoryRepository 구현완료

--- src/repository/BuyHistoryRepository.java
+++ src/repository/BuyHistoryRepository.java
@@ -1,11 +1,24 @@
 package repository;
 
 import domain.Customer;
+import jdbc.DBConnectionManager;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 public class BuyHistoryRepository {
     public void buyProducts(Customer customer) {
+        String sql = "INSERT INTO BUY_HISTORY (BUY_ID, USER_ID) " +
+                "VALUES (BUY_HISTORY_SEQ, ?)";
 
+        try (Connection conn = DBConnectionManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, customer.getUserId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
