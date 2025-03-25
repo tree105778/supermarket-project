407958ba17cf0f1aa7c7020035f16791486b8d87
EunHyeok <secun77@naver.com>
2025-03-25 10:02:37 +0900

    순 수익 제외한 모든 관리자 메소드, UI 추가 완료

--- src/repository/AdminRepository.java
+++ src/repository/AdminRepository.java
@@ -15,6 +15,7 @@ public class AdminRepository {
 
     CustomerRepository customerRepository;
 
+    // 관리자모드에서 모든 buy_history를 날짜기준 최신순으로 모두 리스트로 묶어서 리턴
     public List<buyHistory> searchBuyHistory(){
             List<buyHistory> buyHistoryList = new ArrayList<>();
             String sql = "SELECT * FROM buy_history b JOIN customer c ON b.user_id = c.user_id ORDER BY b.buy_time DESC";
@@ -26,8 +27,8 @@ public class AdminRepository {
                 while(rs.next()){
                     buyHistory temp = new buyHistory();
                     temp.setBuyId(rs.getInt("buy_id"));
-                    // Date 타입을 LocalDateTime으로 변환하는데 문제가 있음.
-                    temp.setBuyTime(rs.getDate("buy_time").toLocalDate().atTime(6,30));
+
+                    temp.setBuyTime(rs.getTimestamp("buy_time").toLocalDateTime());
                     temp.setUser(customerRepository.getCustomerByUserId(rs.getInt("user_id")));
                     temp.setTotalPrice(rs.getInt("total_price"));
                     buyHistoryList.add(temp);
--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -1,18 +1,18 @@
 package service;
 
-import domain.Product;
 import domain.buyHistory;
 import repository.AdminRepository;
+import repository.BuyHistoryRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static ui.CommonUI.*;
 
 public class AdminService {
 
-    ProductService productService;
     AdminRepository adminRepository;
+    ProductService productService;
+    BuyHistoryRepository buyHistoryRepository;
 
     public static void startAdminScreen() {
         System.out.println("================= 관리자 계정입니다. =================");
@@ -23,29 +23,32 @@ public class AdminService {
         System.out.println("### 5. 총매출, 수익 조회");
         System.out.println("### 6. 관리자 모드 종료");
 
-        makeLine();
     }
 
-    // 회원 목록 검색
-
-    // 제품 등록
-
-    // 제품 조회
-
-    // 제품 삭제
-
-    public void start() {
+    public void startAdminUI() {
         while (true) {
             startAdminScreen();
             int selection = inputInteger(">>> ");
 
             switch (selection) {
-                case 1: addProduct(); break;
-                case 2: deleteProduct(); break;
-                case 3: searchAllCustomer(); break;
-                case 4: showBuyHistory(); break;
-                case 5: showProfit(); break;
-                case 6: startCustomerScreen(); break;
+                case 1:
+                    addProduct();
+                    break;
+                case 2:
+                    deleteProduct();
+                    break;
+                case 3:
+                    searchAllCustomer();
+                    break;
+                case 4:
+                    showBuyHistory();
+                    break;
+                case 5:
+                    showProfit();
+                    break;
+                case 6:
+                    startCustomerScreen();
+                    break;
                 default:
                     System.out.println("# 메뉴를 다시 입력하세요!");
 
@@ -53,40 +56,71 @@ public class AdminService {
         }
     }
 
-    private void showProfit() {
 
-    }
+        private void showProfit() {
+            System.out.println("\n============== 관리자 모드입니다. ===============");
+            System.out.println("\n============== 옵션을 선택하세요. ===============");
+            System.out.println("[ 1. 오늘 매출 | 2. 최근 7일 매출 | 3. 최근 30일 매출 | 4. 전 화면으로 돌아가기 ]");
+            int option = inputInteger("### 옵션: ");
+            int profit = 0; String str = "";
+            switch (option) {
+                case 1:
+                    profit = buyHistoryRepository.getTotalPrice(1);
+                    str += "오늘";
+                    break;
+                case 2:
+                    profit = buyHistoryRepository.getTotalPrice(2);
+                    str += "최근 7일";
+                    break;
+                case 3:
+                    profit = buyHistoryRepository.getTotalPrice(3);
+                    str += "최근 30일";
+                    break;
+                case 4:
+                    startAdminUI();
+                    break;
+                default:
+                    System.out.println("# 메뉴를 다시 입력하세요!");
 
-    private void showBuyHistory() {
-        System.out.println("\n============== 관리자 모드입니다. ===============");
-        System.out.println("\n============== 구매 내역 ================");
-        try {
-            List<buyHistory> histories = adminRepository.searchBuyHistory();
-            int count = histories.size();
-            if(count > 0) {
-                System.out.printf("\n======================================= 검색 결과(총 %d건) =======================================\n", count);
-                for (buyHistory history : histories) {
-                    System.out.println(history);
-                }
-            } else {
-                System.out.println("\n### 검색 결과가 없습니다.");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.printf("\n============== %s 총 매출 ================\n", str);
+
+            // 나중에
+            // System.out.println("\n============== 순 수익 ===============");
+
         }
-    }
 
-    private void deleteProduct() {
-        productService.deleteProductData();
-    }
+        private void showBuyHistory() {
+            System.out.println("\n============== 관리자 모드입니다. ===============");
+            System.out.println("\n============== 구매 내역 ================");
+            try {
+                List<buyHistory> histories = adminRepository.searchBuyHistory();
+                int count = histories.size();
+                if(count > 0) {
+                    System.out.printf("\n======================================= 검색 결과(총 %d건) =======================================\n", count);
+                    for (buyHistory history : histories) {
+                        System.out.println(history);
+                    }
+                } else {
+                    System.out.println("\n### 검색 결과가 없습니다.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-    private void addProduct() {
-        productService.insertProductData();
-    }
+        private void deleteProduct() {
+            productService.deleteProductData();
+        }
 
-    private void searchAllCustomer() {
-        productService.searchProductDataForAdmin();
-    }
+        private void addProduct() {
+            productService.insertProductData();
+        }
+
+        private void searchAllCustomer() {
+            productService.searchProductDataForAdmin();
+        }
+
 
 
 }
